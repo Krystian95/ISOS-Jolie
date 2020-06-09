eeef347ba1e0247c70d8402c7bcb304b3ac44fdb
aandreal <94andrealongo@gmail.com>
2020-06-09 18:48:42 +0200

    Update Executable.bat

--- Executable.bat
+++ Executable.bat
@@ -1,8 +1,8 @@
 start "ACME_gestione_ordini" jolie -C "AUTOMATIC_MODE=true" ACME_gestione_ordini.ol
+start "GIS" jolie -C "AUTOMATIC_MODE=true" GIS.ol
 start "ACME_magazzini_MP" jolie -C "AUTOMATIC_MODE=true" ACME_magazzini_MP.ol
 start "ACME_magazzini_MS_1" jolie -C "AUTOMATIC_MODE=true" ACME_magazzini_MS_1.ol
 start "ACME_magazzini_MS_2" jolie -C "AUTOMATIC_MODE=true" ACME_magazzini_MS_2.ol
 start "ACME_magazzini_MS_3" jolie -C "AUTOMATIC_MODE=true" ACME_magazzini_MS_3.ol
 start "rivenditore1" jolie -C "AUTOMATIC_MODE=true" rivenditore1.ol
-start "GIS" jolie -C "AUTOMATIC_MODE=true" GIS.ol
 exit
