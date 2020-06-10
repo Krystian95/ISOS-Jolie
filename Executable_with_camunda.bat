7dad57336bb34c601017fe6ec41da1f9f618faf3
aandreal <94andrealongo@gmail.com>
2020-06-10 10:55:59 +0200

    Added Executable_with_camunda
    Per Andrea

---
+++ Executable_with_camunda.bat
@@ -0,0 +1,11 @@
+cd ..\..\..\Desktop\CamundaEE\
+start start-camunda.bat
+cd ..\..\Documents\Progetti\ISOS-Jolie
+start "ACME_gestione_ordini" jolie -C "AUTOMATIC_MODE=true" ACME_gestione_ordini.ol
+start "GIS" jolie -C "AUTOMATIC_MODE=true" GIS.ol
+start "ACME_magazzini_MP" jolie -C "AUTOMATIC_MODE=true" ACME_magazzini_MP.ol
+start "ACME_magazzini_MS_1" jolie -C "AUTOMATIC_MODE=true" ACME_magazzini_MS_1.ol
+start "ACME_magazzini_MS_2" jolie -C "AUTOMATIC_MODE=true" ACME_magazzini_MS_2.ol
+start "ACME_magazzini_MS_3" jolie -C "AUTOMATIC_MODE=true" ACME_magazzini_MS_3.ol
+start "rivenditore1" jolie -C "AUTOMATIC_MODE=true" rivenditore1.ol
+exit
