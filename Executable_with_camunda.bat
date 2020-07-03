a69e3b30751243f4ff13f0d724b69ab39c176e08
aandreal <94andrealongo@gmail.com>
2020-07-03 20:39:23 +0200

    Update Executable with camunda (added Banca)

--- Executable_with_camunda.bat
+++ Executable_with_camunda.bat
@@ -7,5 +7,6 @@ start "ACME_magazzini_MP" jolie -C "AUTOMATIC_MODE=true" ACME_magazzini_MP.ol
 start "ACME_magazzini_MS_1" jolie -C "AUTOMATIC_MODE=true" ACME_magazzini_MS_1.ol
 start "ACME_magazzini_MS_2" jolie -C "AUTOMATIC_MODE=true" ACME_magazzini_MS_2.ol
 start "ACME_magazzini_MS_3" jolie -C "AUTOMATIC_MODE=true" ACME_magazzini_MS_3.ol
-start "rivenditore1" jolie -C "AUTOMATIC_MODE=true" rivenditore1.ol
+start "Rivenditore1" jolie -C "AUTOMATIC_MODE=true" rivenditore1.ol
+start "Banca" jolie -C "AUTOMATIC_MODE=true" Banca.ol
 exit
